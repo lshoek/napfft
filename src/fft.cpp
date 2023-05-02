ca392f812d7f57c518ea8f36a1a80dc49a06bbe2
lshoek <lesleyvanhoek@hotmail.com>
2023-05-02 11:51:46 +0200

    run some tests

--- src/fft.cpp
+++ src/fft.cpp
@@ -1,24 +1,72 @@
-
 #include "fft.h"
 
 // External includes
-#include <kiss_fft.h>
+#include <kissfft.hh>
+#include <mathutils.h>
 
-// nap::FFTAnalysis run time class definition 
-RTTI_BEGIN_CLASS(nap::FFTAnalysis)
-
+// nap::FFTBuffer run time class definition 
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::FFTBuffer)
+	RTTI_PROPERTY("RequestedFFTSize", &nap::FFTBuffer::mRequestedFFTSize, nap::rtti::EPropertyMetaData::Default)
 RTTI_END_CLASS
+
+RTTI_BEGIN_CLASS(nap::FFTBufferFloat)
+RTTI_END_CLASS
+
+RTTI_BEGIN_CLASS(nap::FFTBufferDouble)
+RTTI_END_CLASS
+
+RTTI_BEGIN_CLASS(nap::FFTBufferInt)
+RTTI_END_CLASS
+
+RTTI_BEGIN_CLASS(nap::FFTBufferShort)
+RTTI_END_CLASS
+
 
 //////////////////////////////////////////////////////////////////////////
 
 
 namespace nap
 {
-	FFTAnalysis::~FFTAnalysis() { }
-
-
-	bool FFTAnalysis::init(utility::ErrorState& errorState)
+	template <typename T>
+	class KissFFTState
 	{
+	public:
+		KissFFTState::KissFFTState(size_t fftSize) :
+			mFFT(fftSize, false) { }
+
+		kissfft<T> mFFT;
+	};
+
+
+	template <typename T>
+	TypedFFTBuffer<T>::TypedFFTBuffer() :
+		FFTBuffer()
+	{ }
+
+
+	template <typename T>
+	bool TypedFFTBuffer<T>::init(utility::ErrorState& errorState)
+	{
+		// Determine optimal number of frequency bins
+		mFFTState = std::make_unique<KissFFTState<T>>(static_cast<size_t>(mRequestedFFTSize));
+
+		std::vector<T> test_samples;
+		test_samples.reserve(2048);
+		for (uint i = 0; i < 2048; i++)
+			test_samples.emplace_back(static_cast<T>(math::random(0.0f, 32.0f)));
+
+		std::vector<std::complex<T>> result(test_samples.size());
+		fft(test_samples, result, errorState);
+
+		return true;
+	}
+
+
+	template <typename T>
+	bool TypedFFTBuffer<T>::fft(const std::vector<T>& sampleBuffer, std::vector<std::complex<T>>& outBuffer, utility::ErrorState& errorState)
+	{		
+		outBuffer.reserve(mRequestedFFTSize);
+		mFFTState->mFFT.transform_real(sampleBuffer.data(), outBuffer.data());
 		return true;
 	}
 }
--- src/fft.h
+++ src/fft.h
@@ -2,22 +2,51 @@
 
 // External Includes
 #include <nap/resource.h>
+#include <nap/numeric.h>
+#include <complex>
 
 namespace nap
 {
+	// Forward declares
+	template <typename T>
+	class KissFFTState;
+
 	/**
-	 * FFTAnalysis
+	 * FFTBuffer
 	 */
-	class NAPAPI FFTAnalysis : public Resource
+	class NAPAPI FFTBuffer : public Resource
 	{
 		RTTI_ENABLE(Resource)
 	public:
-		virtual ~FFTAnalysis();
+		virtual ~FFTBuffer() {};
+		uint mRequestedFFTSize = 1024U;
+	};
+
+
+	template <typename T>
+	class NAPAPI TypedFFTBuffer : public FFTBuffer
+	{
+		RTTI_ENABLE(FFTBuffer)
+	public:
+		TypedFFTBuffer();
+		virtual ~TypedFFTBuffer() {}
 
 		/**
 		* Initialize this object after de-serialization
 		* @param errorState contains the error message when initialization fails
 		*/
 		virtual bool init(utility::ErrorState& errorState) override;
+
+		/**
+		 * 
+		 */
+		bool fft(const std::vector<T>& sampleBuffer, std::vector<std::complex<T>>& outBuffer, utility::ErrorState& errorState);
+
+		std::unique_ptr<KissFFTState<T>> mFFTState;
 	};
+
+	using FFTBufferFloat = TypedFFTBuffer<float>;
+	using FFTBufferDouble = TypedFFTBuffer<double>;
+	using FFTBufferInt = TypedFFTBuffer<int>;
+	using FFTBufferShort = TypedFFTBuffer<int16>;
 }
